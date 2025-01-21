674b399d2b1ed9488bf980239ccf47fe7ee4ffa5
chesnats <nap.cmanolo@gmail.com>
2025-01-21 09:27:38 +0800

    setup of the express js server

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,16 +1,18 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginVue from "eslint-plugin-vue";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,vue}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node, // This should allow 'require' and 'process'
+      },
+    },
+  },
   pluginJs.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  
-
-  
+  ...pluginVue.configs['flat/essential'],
 ];
--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-
+console.log('hello')
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -7,15 +7,16 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/status', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 
 })
 
 app.listen(process.env.PORT || 8081)
+
